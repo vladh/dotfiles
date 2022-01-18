cfa9ff2205cc374d4d134153c3c88978565cc6b9
Vlad-Stefan Harbuz <vlad@vladh.net>
2022-01-18 20:02:35 +0100

    change prompt colours

--- .bin/lib/prompt.sh
+++ .bin/lib/prompt.sh
@@ -44,7 +44,7 @@ if [ -n "$BASH$FISH_VERSION" ]; then
   # character count and mess up scrolling etc.
   # PS1 used to be prefixed with: \[\033[G\]
   # What is that?
-  export PS1="\[$c_blue\]\u \[$c_red\]\h\[$c_blue\] \W\$(git_prompt)\$(venv_prompt)\[$c_blue\]$prompt_char \[$c_reset\]"
+  export PS1="\[$c_blue\]\u \[$c_purple\]\h\[$c_blue\] \W\$(git_prompt)\$(venv_prompt)$prompt_char \[$c_reset\]"
 else
-  export PS1="$c_blue$(whoami) $c_red$(hostname)$c_blue $(basename $(pwd))\$(git_prompt)\$(venv_prompt)$c_blue$prompt_char $c_reset"
+  export PS1="$c_blue$(whoami) $c_red$(hostname)$c_blue $(basename $(pwd))\$(git_prompt)\$(venv_prompt)$prompt_char $c_reset"
 fi
